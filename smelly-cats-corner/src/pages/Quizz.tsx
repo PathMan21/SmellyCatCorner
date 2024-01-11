2e96e1cd375f6480cd5baa0f60feff20ecc1273e
Angelina <julia.angelina.grossi@gmail.com>
2024-01-11 16:48:04 +0100

    Lancement du projet

---
+++ smelly-cats-corner/src/pages/Quizz.tsx
@@ -0,0 +1,33 @@
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from 'react-router';
+import ExploreContainer from '../components/ExploreContainer';
+import './Quizz.css';
+
+const Quizz: React.FC = () => {
+
+    const { name } = useParams<{ name: string; }>();
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>Quizz</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">{name}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <ExploreContainer name={name} />
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default Quizz;
