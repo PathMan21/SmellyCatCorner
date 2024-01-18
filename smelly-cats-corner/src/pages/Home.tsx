7de4e3c7e055b727d951ce2cebd8439a8e6db972
Angelina <julia.angelina.grossi@gmail.com>
2024-01-18 16:38:16 +0100

    debugs d'affichage

--- smelly-cats-corner/src/pages/Home.tsx
+++ smelly-cats-corner/src/pages/Home.tsx
@@ -13,7 +13,6 @@ const Home: React.FC = () => {
     <IonPage>
       <IonContent fullscreen>
         <Menu/>
-          <IonImg className="friends-title-img" src='https://friends-v1ol.onrender.com/img/friends.webp'></IonImg>
       </IonContent>
     </IonPage>
   );
